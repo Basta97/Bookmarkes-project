9c9f53f759d72f053e7af365c22ac9e5c6173456
ahmed <ahmed.b.01.12.97@gmail.com>
2024-11-24 14:52:25 +0200

    first finish

--- js/main.js
+++ js/main.js
@@ -1,36 +1,107 @@
-let bookmarkNameInput ;
-let bookmarkUrlInput ;
-let bookmark={
-    bookmarkName:'',
-    bookmarkUrl :''
-};
+let bookmarkNameInput = document.getElementById('bookmarkName');
+let bookmarkUrlInput = document.getElementById('bookmarkURL');
+let showBookmark = document.getElementById('bookmark-list-show');
 let bookmarkList=[]
-let sumbitB=document.getElementById('add-btn');
+let buttS=document.getElementById('add-btn');
 let show;
-function addB(){
-    bookmarkNameInput = document.getElementById('bookmarkName').value;
-    bookmarkUrlInput = document.getElementById('bookmarkURL').value;
+let nameCh;
+let urlChe;
+let checker =false;
 
-    bookmark={
-        bookmarkName:bookmarkNameInput,
-        bookmarkUrl: bookmarkUrlInput
+
+if(localStorage.getItem('bookmarkList')!==null){
+    bookmarkList=JSON.parse(localStorage.getItem('bookmarkList'));
+    disBookmark();
+}
+
+function addB(){
+    let bookmark={
+        bookmarkName:bookmarkNameInput.value,
+        bookmarkUrl: bookmarkUrlInput.value
     }
     bookmarkList.push(bookmark);
+    localStorage.setItem('bookmarkList', JSON.stringify(bookmarkList));
+    cleatF();
     disBookmark();
 
 
 }
+function cleatF(){
+    bookmarkNameInput.value=null;
+    bookmarkUrlInput.value=null;
+    document.getElementById('bookmarkURL').classList.remove('is-valid');
+    document.getElementById('bookmarkURL').classList.remove('is-invalid');
+    document.getElementById('bookmarkName').classList.remove('is-valid');
+    document.getElementById('bookmarkName').classList.remove('is-invalid');
+}
 function disBookmark(){
-    let i;
-    for( i=0;i<bookmarkList.length;i++){
+    let show=``;
+    for(let i=0;i<bookmarkList.length;i++){
         show+= `<tr>
-              <td>${i}</td>
-              <td>${bookmarkList[i].bookmarkName}</td>
-              <td><a href="https://www.google.com" target="_blank">${bookmarkList[i].bookmarkUrl}</a></td>
-              <td><button class="btn btn-danger delete-btn">Delete</button></td>
+              <td class="p-2">${i+1}</td>
+              <td class="p-2">${bookmarkList[i].bookmarkName}</td>
+              <td class="p-2"><a href="${bookmarkList[i].bookmarkUrl}" target="_blank"><button class="btn btn-danger delete-btn">Visit</button></a></td>
+              <td class="p-2"><button onclick="deleteB(${i})" id="dl-btn" class="btn btn-danger delete-btn">Delete</button></td>
              </tr>`
     }
-    console.log(bookmarkList[i])
+    showBookmark.innerHTML = show;
+    
+    
 }
-sumbitB.addEventListener('click',addB)
-console.log(bookmarkList)
+
+
+function deleteB(index){
+    bookmarkList.splice(index,1);
+    disBookmark();
+    localStorage.setItem('bookmarkList',JSON.stringify(bookmarkList))
+}
+
+function isValidName(){
+     nameCh=/^([a-z]|[A-Z]){4}/.test(bookmarkNameInput.value);
+ 
+   
+    if( nameCh===true){
+        document.getElementById('bookmarkName').classList.remove('is-invalid');
+        document.getElementById('bookmarkName').classList.add('is-valid');
+        return true;
+
+    }
+    else{
+        document.getElementById('bookmarkName').classList.remove('is-valid');
+        document.getElementById('bookmarkName').classList.add('is-invalid');
+        return false;
+    }
+}
+function isValidUrl(){
+
+    urlChe=/^(https?|www.)/.test(bookmarkUrlInput.value); 
+    
+  
+   if( urlChe===true){
+       document.getElementById('bookmarkURL').classList.remove('is-invalid');
+       document.getElementById('bookmarkURL').classList.add('is-valid');
+       checker=urlChe;
+
+   }
+   else{
+       document.getElementById('bookmarkURL').classList.remove('is-valid');
+       document.getElementById('bookmarkURL').classList.add('is-invalid');
+       checker=urlChe;
+   }
+   
+}
+buttS.addEventListener('click',function(){
+    if (checker===false){
+    
+        document.getElementById('layer').classList.remove('d-none')
+    }
+    else{
+        
+        addB()
+        checker =false;
+        
+    }
+});
+document.getElementById('alert-w').addEventListener('click',function(){
+    document.getElementById('layer').classList.add('d-none')
+})   
